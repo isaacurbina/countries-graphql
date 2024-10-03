3bd87ac062b5c3fedcfad697b328f8f581c8c14d
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-10-02 20:09:54 -0500

    1. Setting up project plugins and libraries

--- app/src/main/java/com/iucoding/countriesgraphql/MainActivity.kt
+++ app/src/main/java/com/iucoding/countriesgraphql/MainActivity.kt
@@ -5,14 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.iucoding.countriesgraphql.ui.theme.CountriesGraphQLTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +18,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CountriesGraphQLTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TODO("continue tutorial on 5:18 https://www.youtube.com/watch?v=ME3LH2bib3g")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CountriesGraphQLTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/iucoding/countriesgraphql/ui/theme/Theme.kt
+++ app/src/main/java/com/iucoding/countriesgraphql/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.iucoding.countriesgraphql.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
