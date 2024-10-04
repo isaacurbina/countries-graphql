8b430951e6f473d049b43bdf77ff7b6be15e2718
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-10-04 17:46:22 -0500

    3. Adding composables

---
+++ app/src/main/java/com/iucoding/countriesgraphql/di/AppModule.kt
@@ -0,0 +1,3 @@
+package com.iucoding.countriesgraphql.di
+
+
---
+++ app/src/main/java/com/iucoding/countriesgraphql/presentation/composable/CountryItem.kt
@@ -0,0 +1,54 @@
+package com.iucoding.countriesgraphql.presentation.composable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.iucoding.countriesgraphql.domain.model.SimpleCountry
+
+@Composable
+fun CountryItem(
+    country: SimpleCountry,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = country.emoji,
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = country.name,
+                fontSize = 24.sp
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = country.capital)
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+private fun CountryItemPreview() {
+    CountryItem(
+        country = SimpleCountry(
+            code = "US",
+            name = "United States",
+            emoji = "\uD83C\uDDFA\uD83C\uDDF8",
+            capital = "Washington D.C."
+        )
+    )
+}
